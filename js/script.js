8a44703b6eb1a40f02120c29479dd46c430fc981
Alethepata <alessia.patane@gmail.com>
2023-09-12 15:10:09 +0200

    [Add] Script dadi

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const registrati = ['a', 'e', 'i', 'o', 'u'];
+const emailDaAggiungere = 1;
+
+for (let i = 0; i < emailDaAggiungere; i++){
+  const email = prompt('Digitare email');
+  let emailcorretta = false;
+  
+  for (let e = 0; e < registrati.length; e++){
+    const utentiregistrati = registrati[e];
+    if(utentiregistrati === email){
+      emailcorretta = true;
+          console.log('accesso');
+
+    }
+  }
+  if(! emailcorretta){
+    console.log('negativo');
+}
+}
