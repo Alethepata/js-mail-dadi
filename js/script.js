ab6d12a40221668f856fa70727096c4bd3e48a74
Alethepata <alessia.patane@gmail.com>
2023-09-12 15:13:21 +0200

    [Edit] email

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
+
+// EMAIL
+
 const registrati = ['a', 'e', 'i', 'o', 'u'];
 const emailDaAggiungere = 1;
 
 for (let i = 0; i < emailDaAggiungere; i++){
   const email = prompt('Digitare email');
   let emailcorretta = false;
-  
+
   for (let e = 0; e < registrati.length; e++){
     const utentiregistrati = registrati[e];
     if(utentiregistrati === email){
